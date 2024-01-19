3c6ce43bceedbdb12c33f8764fa24f09b683523b
Timothy J. Thacker <tjt607@gmail.com>
2024-01-18 22:00:41 -0500

    web automation around letcode site

---
+++ tests/letcode.spec.ts
@@ -0,0 +1,29 @@
+//test automation practice site 
+// https://letcode.in/test
+
+import { test, expect } from '@playwright/test';
+
+test('can send keys', async ({ page }) => {
+    // locators
+    const fullNameInput = page.locator('xpath=//input[@id="fullName"]');
+    const joinInput = page.locator('xpath=//input[@id="join"]');
+    const getMeInput = page.locator('xpath=//input[@id="getMe"]');
+   
+    
+    // actions
+    await page.goto('https://letcode.in/edit');
+    await fullNameInput.fill('Timothy Thacker');
+    // Get the current value of the input field
+    const currentInputValue = await joinInput.inputValue();
+    await joinInput.fill(`${currentInputValue} appending some text and then tabbing out!`);
+    await page.keyboard.press('Tab');
+    await getMeInput.getAttribute('value');
+    //true
+    expect(await getMeInput.getAttribute('value')).toBe('ortonikc');
+    //false 
+    //expect(await getMeInput.getAttribute('value')).toBe('hey!');
+
+  return;
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
